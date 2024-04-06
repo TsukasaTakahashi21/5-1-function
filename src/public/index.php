480835a321793d851f666540cfdfba9ee7a4e7b7
“tsukasatakahashi21” <tukaburu13@gmail.com>
2024-04-06 10:38:38 +0900

    fix: エラー修正

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,33 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<!-- 課題２ -->
+
+<?php  
+$spendings = ["apple" => 100, "orange" => 150];
+echo findTotalSpendings($spendings);
+
+function findTotalSpendings($spendings)
+{
+    $totalSpendings = 0;
+    foreach ($spendings as $spending) {
+        $totalSpendings += $spending;
+    }
+    return $totalSpendings;
+}
+?>
+
+
+<!-- 課題２ -->
+
+<?php 
+$spendings = ["apple" => 100, "orange" => 150];
+$incomes = ["okozukai" => 10000, "partTimeSalary" => 35000];
+echo findTotalIncomes($incomes);
+
+function findTotalIncomes($incomes)
+{
+    $totalIncomes = 0;
+    foreach ($incomes as $income) {
+        $totalIncomes += $income;
+    }
+    return $totalIncomes;
+}
+
